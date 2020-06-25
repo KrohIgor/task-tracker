4343104cf3c5db04022e7e1535cb9cb36b9398c1
ihor <krohmal.igor777@gmail.com>
2020-06-25 16:30:51 +0300

    first commit

---
+++ src/test/java/test/task/task/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package test.task.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
